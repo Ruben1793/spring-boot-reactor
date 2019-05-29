b4eae52726147676f1c7dd5aad5203ddaf877091
Ruben Ojeda <ruben.ojed9a3@gmail.com>
2019-05-28 22:11:52 -0500

    El metodo subscribe()

--- src/main/java/com/bolsadeideas/springboot/reactor/springboot/reactor/app/SpringBootReactorApplication.java
+++ src/main/java/com/bolsadeideas/springboot/reactor/springboot/reactor/app/SpringBootReactorApplication.java
@@ -1,5 +1,7 @@
 package com.bolsadeideas.springboot.reactor.springboot.reactor.app;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -8,15 +10,24 @@ import reactor.core.publisher.Flux;
 @SpringBootApplication
 public class SpringBootReactorApplication implements CommandLineRunner {
 
+    private Logger Log = LoggerFactory.getLogger(SpringBootReactorApplication.class);
+
     public static void main(String[] args) {
         SpringApplication.run(SpringBootReactorApplication.class, args);
     }
 
     @Override
     public void run(String... args) throws Exception {
-        Flux<String> nombres = Flux.just("Andres", "Pedro", "Diego" ,"Juan")
-                .doOnNext(System.out::println);
-        nombres.subscribe();
+        Flux<String> nombres = Flux.just("Andres", "Pedro", "" ,"Juan")
+                .doOnNext( e -> {
+                    if (e.isEmpty()){
+                        throw new RuntimeException("Nombres no pueden ser vacios");
+                    }
+                    System.out.println(e);
+                });
+
+        nombres.subscribe(e -> Log.info(e),
+                error -> Log.error(error.getMessage()));
 
     }
 }
