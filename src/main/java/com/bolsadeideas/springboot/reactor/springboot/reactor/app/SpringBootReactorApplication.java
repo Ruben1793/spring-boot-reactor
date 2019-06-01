7acb99890c0eb926ffdc1451b6eb596ff0ba8914
Ruben Ojeda <ruben.ojed9a3@gmail.com>
2019-05-31 22:55:02 -0500

    El operador map

--- src/main/java/com/bolsadeideas/springboot/reactor/springboot/reactor/app/SpringBootReactorApplication.java
+++ src/main/java/com/bolsadeideas/springboot/reactor/springboot/reactor/app/SpringBootReactorApplication.java
@@ -1,5 +1,6 @@
 package com.bolsadeideas.springboot.reactor.springboot.reactor.app;
 
+import com.bolsadeideas.springboot.reactor.springboot.reactor.app.models.Usuario;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.boot.CommandLineRunner;
@@ -18,15 +19,20 @@ public class SpringBootReactorApplication implements CommandLineRunner {
 
     @Override
     public void run(String... args) throws Exception {
-        Flux<String> nombres = Flux.just("Andres", "Pedro", "Maria" ,"Juan")
-                .doOnNext( e -> {
-                    if (e.isEmpty()){
+        Flux<Usuario> nombres = Flux.just("Andres", "Pedro", "Maria", "Juan")
+                .map(nombre ->  new Usuario(nombre.toUpperCase(), null))
+                .doOnNext(usuario ->{
+                    if (usuario == null){
                         throw new RuntimeException("Nombres no pueden ser vacios");
                     }
-                    System.out.println(e);
+                    System.out.println(usuario.toString());
+                }).map(usuario -> {
+                    String nombre =  usuario.getNombre().toLowerCase();
+                    usuario.setNombre(nombre);
+                    return usuario;
                 });
 
-        nombres.subscribe(e -> Log.info(e),
+        nombres.subscribe(usuario -> Log.info(usuario.getNombre()),
                 error -> Log.error(error.getMessage(),
                         (Runnable) () -> {
                             Log.info("Ha finalizado la ejecucion del observable con exito!");
